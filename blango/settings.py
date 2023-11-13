856f31f7883a50510defaf4a057d6856109efe26
Christian Albrecht <christian.albrecht@bo3f.ch>
2023-11-13 21:01:15 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -59,6 +59,7 @@ class Dev(Configuration):
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
         'rest_framework',
+        'rest_framework.authtoken',
     ]
 
     MIDDLEWARE = [
@@ -130,6 +131,13 @@ class Dev(Configuration):
         },
     ]
 
+    REST_FRAMEWORK = {
+      "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+      ]
+    }
 
     # Internationalization
     # https://docs.djangoproject.com/en/3.2/topics/i18n/
